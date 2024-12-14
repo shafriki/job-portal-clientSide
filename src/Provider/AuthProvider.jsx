00bcf69cdd863fb6cf22af7f21b1f6fd117b0cdb
shafriki <shafriki.cse@gmail.com>
2024-12-14 08:11:15 +0600

    auth probider login problem solved

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,46 +1,44 @@
 import React, { useContext, useEffect, useState } from 'react';
 import AuthCntext from './AuthContext';
-import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from '../Firebase/firebase.config';
 
-const AuthProvider = ({children}) => {
-
+const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) => {
         setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
-    }
+    };
 
     const signInUser = (email, password) => {
         setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
-    }
+    };
 
     const signOutUser = () => {
         setLoading(true);
-        return signOutUser(auth);
-    }
+        return signOut(auth)
+            .finally(() => setLoading(false));
+    };
 
-
-    useEffect ( () =>{
-       const unsubscribe = onAuthStateChanged(auth,currentUser => {
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             setLoading(false);
-            console.log('state camptured:',currentUser);
-        })
-        return () => {
-            unsubscribe();
-        }
-    }, [])
+            console.log('Auth state captured:', currentUser);
+        });
+        return () => unsubscribe();
+    }, []);
 
     const authInfo = {
         user,
         loading,
         createUser,
         signInUser,
-    }
+        signOutUser,
+    };
 
     return (
         <AuthCntext.Provider value={authInfo}>
