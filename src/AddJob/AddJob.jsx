db4ba8e0a896972c465c8e111f566a5ab6a149c8
shafriki <shafriki.cse@gmail.com>
2024-12-17 18:07:13 +0600

    added new feature

--- src/AddJob/AddJob.jsx
+++ src/AddJob/AddJob.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const AddJob = () => {
     return (
@@ -49,12 +50,74 @@ const AddJob = () => {
                     />
                 </div>
 
-                <button type="submit" className="w-full btn bg-blue-500 text-white p-2 rounded-md">
-                    Add Job
-                </button>
+                <StyledButton type="submit">Add Job</StyledButton>
             </form>
         </div>
     );
 };
+
+const StyledButton = styled.button`
+   --border-radius: 15px;
+   --border-width: 4px;
+   appearance: none;
+   position: relative;
+   padding: 1em 2em;
+   border: 0;
+   background-color: #212121;
+   font-family: "Roboto", Arial, "Segoe UI", sans-serif;
+   font-size: 18px;
+   font-weight: 500;
+   color: #fff;
+   z-index: 2;
+   cursor: pointer;
+
+   &::after {
+      --m-i: linear-gradient(#000, #000);
+      --m-o: content-box, padding-box;
+      content: "";
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      padding: var(--border-width);
+      border-radius: var(--border-radius);
+      background-image: conic-gradient(
+         #488cfb,
+         #29dbbc,
+         #ddf505,
+         #ff9f0e,
+         #e440bb,
+         #655adc,
+         #488cfb
+      );
+      -webkit-mask-image: var(--m-i), var(--m-i);
+      mask-image: var(--m-i), var(--m-i);
+      -webkit-mask-origin: var(--m-o);
+      mask-origin: var(--m-o);
+      -webkit-mask-clip: var(--m-o);
+      mask-composite: exclude;
+      -webkit-mask-composite: destination-out;
+      filter: hue-rotate(0);
+      animation: rotate-hue linear 500ms infinite;
+      animation-play-state: paused;
+   }
+
+   &:hover::after {
+      animation-play-state: running;
+   }
+
+   @keyframes rotate-hue {
+      to {
+         filter: hue-rotate(1turn);
+      }
+   }
+
+   box-sizing: border-box;
+
+   &:active {
+      --border-width: 5px;
+   }
+`;
 
 export default AddJob;
